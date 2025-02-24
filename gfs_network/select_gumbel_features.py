beb864ba7bae50aa9bf2a0923e96085c3c5f2bd2
wwydmanski <wwydmanski@gmail.com>
2025-02-24 02:17:00 -0500

    feat: add regression

--- gfs_network/select_gumbel_features.py
+++ gfs_network/select_gumbel_features.py
@@ -4,20 +4,53 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import tqdm
+from typing import Literal
 
-def select_gumbel_features(X_train, y_train, device="cpu", verbose=False, temperature_decay=0.995, epochs=300, batch_size=1):
-    balance = (y_train.sum(axis=0) / len(y_train)).flip(0)
-
-    network = FeatureSelectionNetwork(X_train.shape[1], 32, len(balance)).to(device)
+def select_gumbel_features(
+    X_train,
+    y_train,
+    device="cpu",
+    verbose=False,
+    temperature_decay=0.997,
+    epochs=300,
+    batch_size=1,
+    fs_balance=1.0,
+    target_features_mode: Literal["auto", "target", "raw"] = "auto",
+    mode: Literal["classification", "regression"] = "classification",
+):
+    if mode == "classification":
+        balance = (y_train.sum(axis=0) / len(y_train)).flip(0)
+        network = FeatureSelectionNetwork(X_train.shape[1], 32, len(balance)).to(device)
+        criterion = nn.CrossEntropyLoss(weight=balance.to(torch.float32).to(device))
+    elif mode == "regression":
+        network = FeatureSelectionNetwork(X_train.shape[1], 32, 1).to(device)
+        criterion = nn.MSELoss()
+    else:
+        raise ValueError(f"Invalid mode: {mode}")
 
     trainloader = DataLoader(list(zip(X_train, y_train)), batch_size=batch_size)
 
-    criterion = nn.CrossEntropyLoss(weight=balance.to(torch.float32).to(device))
-    optimizer = optim.Adam([
-        {"params": network.fc1.parameters(), "lr": 3e-3},
-        {"params": network.cont.parameters(), "lr": 3e-4},
-    ])
+    optimizer = optim.Adam(
+        [
+            {"params": network.fc1.parameters(), "lr": 4e-3},
+            {"params": network.cont.parameters(), "lr": 3e-4},
+        ]
+    )
     temperature = 2.0
+    
+    if target_features_mode == "auto":
+        if X_train.shape[1] > 10000:
+            target_features_mode = "raw"
+        else:
+            target_features_mode = "target"
+        
+    if target_features_mode == "target":
+        target_features = 2
+        target_features = target_features / X_train.shape[1]
+        
+    elif target_features_mode == "raw":
+        target_features = None
+
 
     for epoch in tqdm.trange(epochs, disable=not verbose):
         total = 0
@@ -29,7 +62,15 @@ def select_gumbel_features(X_train, y_train, device="cpu", verbose=False, temper
 
             optimizer.zero_grad()
             output, selected_no = network(X_.float(), temperature=temperature)
-            loss = criterion(output, y_.argmax(axis=1)) + selected_no
+            if mode == "classification":
+                proper_y = y_.argmax(axis=1)
+            elif mode == "regression":
+                proper_y = y_
+                
+            if target_features is not None:
+                loss = criterion(output, proper_y) + fs_balance * (selected_no - target_features) ** 2
+            else:
+                loss = criterion(output, proper_y) + fs_balance * selected_no
             loss.backward()
             optimizer.step()
             epoch_loss += loss.item()
@@ -38,7 +79,7 @@ def select_gumbel_features(X_train, y_train, device="cpu", verbose=False, temper
             real = torch.argmax(y_, axis=1)
             total += y_.size(0)
             correct += (predicted == real).sum().item()
-            
+
             if epoch > epochs:
                 temperature = 0
                 network.fc1.requires_grad_(False)
@@ -53,5 +94,5 @@ def select_gumbel_features(X_train, y_train, device="cpu", verbose=False, temper
         features = gumbel_sigmoid(features, tau=0, hard=True)
         features = features.cpu().detach().numpy()
         features_sum += features.sum(axis=0)
-        
+
     return features_sum.numpy(), network
--- gfs_network/sklearn_interface.py
+++ gfs_network/sklearn_interface.py
@@ -1,27 +1,29 @@
 from .select_gumbel_features import select_gumbel_features
 import torch
+from typing import Literal
 
 
 class GFSNetwork:
     def __init__(
         self,
-        threshold: float = 0.01,
-        device: str = "cpu",
-        verbose: bool = False,
+        batch_size=1,
         temperature_decay: float = 0.997,
         epochs: int = 200,
-        batch_size=8
+        balance: float = 1.0,
+        device: str = "cpu",
+        verbose: bool = False,
+        mode: Literal["classification", "regression"] = "classification",
     ) -> None:
         """Perform feature selection using GFSNetwork.
 
         Args:
-            threshold (float, optional): Keep features with scores above this percentile score. Defaults to 0.01.
-            device (str, optional): Device to use. Defaults to "cpu".
-            verbose (bool, optional): Verbosity. Defaults to False.
+            batch_size (int, optional): Batch size. Larger batch size speeds up training, but makes feature selection less aggresive. Defaults to 1.
             temperature_decay (float, optional): Temperature decay. Defaults to 0.997.
             epochs (int, optional): Number of epochs. Defaults to 200.
+            balance (float, optional): Balance between classification and feature selection. Larger value puts more weight on feature selection. Defaults to 1.0.
+            device (str, optional): Device to use. Defaults to "cpu".
+            verbose (bool, optional): Verbosity. Defaults to False.
         """
-        self.threshold = threshold
         self.scores_ = None
         self.device = device
         self.verbose = verbose
@@ -29,17 +31,27 @@ class GFSNetwork:
         self.epochs = epochs
         self.network = None
         self.batch_size = batch_size
+        self.balance = balance
+        self.mode = mode
 
-    def fit(self, X, y, scale=True):
+    def fit(
+        self,
+        X,
+        y,
+        scale=True,
+        target_features_mode: Literal["auto", "target", "raw"] = "raw",
+    ):
         # cast to torch
-        if type(X) != torch.Tensor:
+        if type(X) is not torch.Tensor:
             X = torch.tensor(X)
             y = torch.tensor(y)
 
         # assert that y is one-hot encoded
-        if len(y.shape) == 1:
+        if len(y.shape) == 1 and self.mode == "classification":
             y = torch.nn.functional.one_hot(y.to(int))
-
+        elif len(y.shape) == 1 and self.mode == "regression":
+            y = y.view(-1, 1).to(torch.float32)
+            
         if scale:
             # perform whitening
             X = (X - X.mean(0)) / (X.std(0) + 1e-6)
@@ -51,7 +63,10 @@ class GFSNetwork:
             self.verbose,
             temperature_decay=self.temperature_decay,
             epochs=self.epochs,
-            batch_size=self.batch_size
+            batch_size=self.batch_size,
+            fs_balance=self.balance,
+            target_features_mode=target_features_mode,
+            mode=self.mode,
         )
         self.ranking_ = self.scores_.argsort()[::-1]
         return self
